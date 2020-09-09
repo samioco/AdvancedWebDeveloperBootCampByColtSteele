5b3b7a51a01a8fa7b9935fd27e92175228469913
Sammy <samio1221@gmail.com>
2020-09-09 01:13:11 -0700

    prototype inheritance recap

--- Section12_ObjectOrientedProgrammingWithJavaScript/inheritanceExercise.js
+++ Section12_ObjectOrientedProgrammingWithJavaScript/inheritanceExercise.js
@@ -36,3 +36,17 @@ function Motorcycle(){
 
 Motorcycle.prototype = Object.create(Vehicle.prototype);
 Motorcycle.prototype.constructor = Motorcycle;
+
+// RECAP
+
+// Every time the new keyword is used, a link between the object
+// created and the prototype property of the constructor is established - this link can be accessed using __proto__
+
+// The prototype object contains a property called constructor, which points back to the constructor function
+
+// To share properties and methods for objects created by a constructor function,
+// place them in the prototype as it is most efficient
+
+// To pass methods and properties from one prototype object to another, we can use inheritance
+// which involves setting the prototype property to be a newly created object using
+// Object.create() and resetting the constructor property
